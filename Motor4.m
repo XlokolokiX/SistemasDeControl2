b4dd35f5133b1486fb5f6606ce0c405072d5b109
XlokolokiX <60261460+XlokolokiX@users.noreply.github.com>
2024-04-05 17:43:59 -0300

    Add files via upload

--- Motor4.m
+++ Motor4.m
@@ -11,31 +11,31 @@
 %%
 
 clc; clear all; close all;
-
-Laa = 366e-6; J = 5e-9; Ra = 55.6; Bm = 0; Ki = 6.49e-3; Km = 6.53e-3;     %Parámetros del motor.
-Ei = 12;                                                                   %Tensión de entrada
-
+%PARÁMETROS DEL MOTOR------------------------------------------------------
+Laa = 366e-6; J = 5e-9; Ra = 55.6; Bm = 0; Ki = 6.49e-3; Km = 6.53e-3;     
+Ei = 12;                                                                   
+%--------------------------------------------------------------------------
+%MODELADO EN ESP. ESTADO---------------------------------------------------
 %Variables de estados, x1 = ia; x2 = wr; x3 = theta; u = Va, Tl
 %Sistema M I S O
-
 A = [-Ra/Laa -Km/Laa 0 ; Ki/J -Bm/J 0 ; 0 1 0]
 B = [1/Laa 0 ; 0 -1/J ; 0 0]
 C = [0 1 0]                                                                %Salida omega_R
 D = [0 0]
-
+%--------------------------------------------------------------------------
 %Datos para la simulación
-deltaT = 10e-6;                                                           %Paso para Euler
+deltaT = 10e-6;                                                            %Paso para Euler
 tF = 5;                                                                    %Tiempo final para la simulación
 N = floor(tF/deltaT)                                                       %N puntos a simular
 
-%Condiciones Iniciles
+%CONDICIONES INICIALES-----------------------------------------------------
 ia(1) = 0;
 wr(1) = 0;
 theta(1) = 0;
 x = [ia(1) wr(1) theta(1)]';
 xop = [0 0 0]';
-
-%Simulación
+%--------------------------------------------------------------------------
+%SIMULACIÓN----------------------------------------------------------------
 t = 0:deltaT:N*deltaT;
 u = Ei*ones(1, length(t));                                                 %Entrada de tensión Va
 TORQUE_MAX = 0.0014
@@ -46,7 +46,7 @@ TORQUE_MAX = 0.0014
     %tL(tL>TORQUE_MAX) = TORQUE_MAX;
     %tL(tL<0) = 0;
     %plot(t, tL); grid;
-
+    
 for i=1 : N
     xp = A*(x-xop) + B*[u(i) tL(i)]';
     x = x + xp*deltaT;                                                     %Euler                                                         
@@ -54,29 +54,18 @@ for i=1 : N
     ia(i+1) = x(1);
     theta(i+1) = x(3);
 end
-
-%GRÁFICAS
+%--------------------------------------------------------------------------
+%GRÁFICAS------------------------------------------------------------------
 subplot(4,1,1)
 plot(t, wr); grid;
-title('Motor')
-xlabel('Tiempo [s]');
-ylabel('Velocidad angular [Rad/s]');
-
+title('Motor'); xlabel('Tiempo [s]'); ylabel('Velocidad angular [Rad/s]');
 subplot(4,1,2)
 plot(t, ia); grid;
-title('Armadura')
-xlabel('Tiempo [s]');
-ylabel('Corriente [A]');
-
+title('Armadura'); xlabel('Tiempo [s]'); ylabel('Corriente [A]');
 subplot(4,1,3)
 plot(t, theta);
-title('Motor')
-xlabel('Tiempo [s]');
-ylabel('Angulo [Rad]');
-
+title('Motor');xlabel('Tiempo [s]'); ylabel('Angulo [Rad]');
 subplot(4,1,4)
 plot(t, tL*100*100, t, u);
-title('Entradas')
-xlabel('Tiempo [s]');
-ylabel('[m.N.cm][Volts]');
-legend('Perturbación','Tensión Armadura')
+title('Entradas'); xlabel('Tiempo [s]'); ylabel('[m.N.cm][Volts]'); legend('Perturbación','Tensión Armadura')
+%--------------------------------------------------------------------------
--- RLC23.m
+++ RLC23.m
@@ -12,59 +12,62 @@
 %%
 clear all; clc; close all;
 
-%Lectura de los datos del excel
+%LECTURA EXCEL-------------------------------------------------------------
 excel = xlsread('Curvas_Medidas_RLC_2024.xls'); 
 t = excel(1:end,1);
 I = excel(1:end,2);
 Vc = excel(1:end,3);
 u = excel(1:end,4);
-
-Ei = 12;
-
-%Gráfica de los datos
+Ei = max(u);
+%--------------------------------------------------------------------------
+%GRÁFICA DEL EXCEL---------------------------------------------------------
+figure(1)
 subplot(3,1,1)
 plot(t,u);
-title('Entrada')
-xlabel('Tiempo [s]');
-ylabel('Tensión [V]');
+title('Entrada'); xlabel('Tiempo [s]'); ylabel('Tensión [V]');
 subplot(3,1,2)
 plot(t,Vc);
-title('Capacitor')
-xlabel('Tiempo [s]');
-ylabel('Tensión [V]');
+title('Capacitor'); xlabel('Tiempo [s]'); ylabel('Tensión [V]'); 
 subplot(3,1,3)
 plot(t,I);
-title('Corriente')
-xlabel('Tiempo [s]');
-ylabel('Corriente [I]');
-%Se aprecia un sistema de 2do Orden sin sobrepasamiento
+title('Corriente'); xlabel('Tiempo [s]'); ylabel('Corriente [I]');
+%--------------------------------------------------------------------------
+%MÉTODO DE CHEN
+%ALGORITMO DE BÚSQUEDA DE LOS PUNTOS---------------------------------------
+tI_Inicio = 0.0105;
+tI_Delta = 0.0005;
+tVc_Inicio = 0.011;
+tVc_Delta = 0.001;
+[~ , Ind1_I] = min(abs(t-tI_Inicio))
+[~ , Ind2_I] = min(abs(t-(tI_Inicio + tI_Delta)))
+[~ , Ind3_I] = min(abs(t-(tI_Inicio + 2*tI_Delta)))
+[~ , Ind1_Vc] = min(abs(t-tVc_Inicio))
+[~ , Ind2_Vc] = min(abs(t-(tVc_Inicio + tVc_Delta)))
+[~ , Ind3_Vc] = min(abs(t-(tVc_Inicio + 2*tVc_Delta)))
+t1_Vc = excel(Ind1_Vc,1); Vc1 = excel(Ind1_Vc,3);
+t2_Vc = excel(Ind2_Vc,1); Vc2 = excel(Ind2_Vc,3);
+t3_Vc = excel(Ind3_Vc,1); Vc3 = excel(Ind3_Vc,3);
+t1_I = excel(Ind1_I,1); I1 = excel(Ind1_I,2);
+t2_I = excel(Ind2_I,1); I2 = excel(Ind2_I,2);
+t3_I = excel(Ind3_I,1); I3 = excel(Ind3_I,2);
 
-
-close all;
-%Método de Chen
-%Identificación de sistemas de 2do Orden basado en la respuesta al escalón
-%Elijo 3 Puntos (equisdistantes) para aplicar el método
-delta_V = 2
-inicio_V = 104
-t1_Vc = excel(inicio_V,1); Vc1 = excel(inicio_V,3)/Ei; 
-t2_Vc = excel(inicio_V + delta_V,1); Vc2 = excel(inicio_V + delta_V,3)/Ei;
-t3_Vc = excel(inicio_V + delta_V*2,1); Vc3 = excel(inicio_V + delta_V*2,3)/Ei;
-
-delta_I = 1
-inicio_I = 102
-t1_I = excel(inicio_I,1); I1 = excel(inicio_I,2)/Ei;
-t2_I = excel(inicio_I + delta_I,1); I2 = excel(inicio_I + delta_I,2)/Ei;
-t3_I = excel(inicio_I + delta_I*2,1); I3 = excel(inicio_I + delta_I*2,2)/Ei;
-
-%Ganancias
+%Visualización de los puntos
+figure(2);
+zoom = [90 150];
+subplot(2,1,1)
+plot(t(zoom(1):zoom(2)) ,I(zoom(1):zoom(2)), [t1_I t2_I t3_I],[I1 ,I2 ,I3],'+'); grid;
+title('Corriente'); xlabel('Tiempo [s]'); ylabel('Corriente [I]');
+subplot(2,1,2)
+plot( t(zoom(1):zoom(2)) ,Vc(zoom(1):zoom(2)) , [t1_Vc t2_Vc t3_Vc],[Vc1 ,Vc2 ,Vc3],'+'); grid;
+title('Capacitor'); xlabel('Tiempo [s]'); ylabel('Tensión [V]'); ylim([0 11]);
+%--------------------------------------------------------------------------
+%GANANCIAS-----------------------------------------------------------------
 K_Vc = excel(500,3)/Ei;     K_I = excel(498,2)/Ei;
-
-%Defino las k correspondientes para los puntos tomados
-k1_V = (Vc1/K_Vc) - 1;   k1_I= (I1/K_I) - 1;
-k2_V = (Vc2/K_Vc) - 1;   k2_I= (I2/K_I) - 1;
-k3_V = (Vc3/K_Vc) - 1;   k3_I= (I3/K_I) - 1;
-
-%Tomando la EC23 del método PARA VC
+k1_V = (Vc1/(Ei*K_Vc)) - 1;   k1_I= (I1/(Ei*K_I)) - 1;
+k2_V = (Vc2/(Ei*K_Vc)) - 1;   k2_I= (I2/(Ei*K_I)) - 1;
+k3_V = (Vc3/(Ei*K_Vc)) - 1;   k3_I= (I3/(Ei*K_I)) - 1;
+%--------------------------------------------------------------------------
+%RESOLVIENDO (EC23) PARA VC
 b_V = 4*k1_V^3*k3_V - 3*k1_V^2*k2_V^2 - 4*k2_V^3 + k3_V^2 + 6*k1_V*k2_V*k3_V
 alpha1_V = (k1_V*k2_V + k3_V - sqrt(b_V))/(2*(k1_V^2 + k2_V))
 alpha2_V = (k1_V*k2_V + k3_V + sqrt(b_V))/(2*(k1_V^2 + k2_V))
@@ -76,8 +79,8 @@ T2_V = -(t1_Vc - 0.01)/log(alpha2_V)
 T3_V = (beta_V*(T1_V - T2_V)) + T1_V
 
 G_V = tf(K_Vc*[T3_V 1], conv([T1_V 1],[T2_V 1]) )
-
-%Tomando la EC23 del método PARA I
+%--------------------------------------------------------------------------
+%RESOLVIENDO (EC23) PARA I
 b_I = 4*k1_I^3*k3_I - 3*k1_I^2*k2_I^2 - 4*k2_I^3 + k3_I^2 + 6*k1_I*k2_I*k3_I
 alpha1_I = (k1_I*k2_I + k3_I - sqrt(b_I))/(2*(k1_I^2 + k2_I))
 alpha2_I = (k1_I*k2_I + k3_I + sqrt(b_I))/(2*(k1_I^2 + k2_I))
@@ -89,43 +92,35 @@ T2_I = -(t1_I - 0.01)/log(alpha2_I)
 T3_I = (beta_I*(T1_I - T2_I)) + T1_I
 
 G_I = tf( K_I*[T3_I 1], conv([T1_I 1],[T2_I 1]) )
-
-%Se compara la FT obtenida con la del excel:
-[Vobt, tobt] = lsim(G_V, u, t);
-[Iobt, tobt] = lsim(G_I, u, t);
-
-subplot(3,1,1)
-plot(t,u);
-title('Entrada')
-xlabel('Tiempo [s]');
-ylabel('Tensión [V]');
-subplot(3,1,2)
-plot(t,Vc,t,Vobt);
-title('Capacitor')
-xlabel('Tiempo [s]');
-ylabel('Tensión [V]');
-subplot(3,1,3)
-plot(t,I,t,Iobt);
-title('Corriente')
-xlabel('Tiempo [s]');
-ylabel('Corriente [I]');
-
-%De la función de transferencia sacamos
+%--------------------------------------------------------------------------
+%OBTENCIÓN DE PARÁMETROS---------------------------------------------------
 C = G_I.num{1}(2)
 L = (G_I.den{1}(1))/C
 R = (G_I.den{1}(2))/C
-
-%Matrices
+%--------------------------------------------------------------------------
+%COMPARACIÓN DE GRAFOS-----------------------------------------------------
+[Vobt, tobt] = lsim(G_V, u, t);
+[Iobt, tobt] = lsim(G_I, u, t);
+figure(3)
+subplot(3,1,1)
+plot(t,u);
+title('Entrada'); xlabel('Tiempo [s]'); ylabel('Tensión [V]');
+subplot(3,1,2)
+plot(t,Vc,t,Vobt);
+title('Capacitor');xlabel('Tiempo [s]'); ylabel('Tensión [V]');
+subplot(3,1,3)
+plot(t,I,t,Iobt);
+title('Corriente'); xlabel('Tiempo [s]'); ylabel('Corriente [I]');
+%--------------------------------------------------------------------------
+%MATRICES------------------------------------------------------------------
 A = [-R/L -1/L; 1/C 0];
 B = [1/L; 0];
 C = [1 0];
 D = 0;
 
-%Definicion de la ecuación de estado y de salida (salida de corriente)
 G1 = ss(A,B,C,D);
-[yout,yt] = lsim(G1,(u),t);
+[Icomp,t] = lsim(G1,u,t);
 
-figure(6)
-plot(yt, yout,'b');grid on; hold on;
-plot( valores(:,1), valores(:,2),'r' ); title('Comparación de corriente');
-legend({'i(t) aproximada con valores RLC calculados','i(t) de excel'},'Location','southeast')
+figure(4)
+plot(t, Icomp, t, I);grid on; hold on; title('Comparación de corriente');
+legend({'i(t)RLC calculados','i(t) de excel'},'Location','southeast')
