f9589868f9f4397d070d882e53fad7b43951c8e2
XlokolokiX <60261460+XlokolokiX@users.noreply.github.com>
2024-06-03 21:50:05 -0300

    Add files via upload

--- ActividadPrácticaN2/Item2.m
+++ ActividadPrácticaN2/Item2.m
@@ -22,38 +22,154 @@ clc; clear all; close all;
 %MODELADO DEL SISTEMA EN EE. (Contínuo)
 a = 0.07; w = 9; b = 5; c = 150;
 
-A_c = [a   -a   0  0;       %x1 = angulo de cabeceo
-       -w^2 w^2 0  0;       %x2 = angulo
-       0    c   0  0;       %x3 = altura
-       0    0   0  1]       %x4 = theta_p
+A_c = [-a   a   0  0;       %x1 = alpha (ángulo con la horizontal)
+       0    0   1  0;       %x2 = phi    (ángulo de cabeceo)
+       w^2 -w^2 0  0;       %x3 = phi_p
+       c    0   0  0]       %x4 = altura
    
 B_c =  [0;
         0;
-        c;
+        b*w^2;
         0]
 
-C_c = [0   0   1   0]
+C_c = [0   0   0   1;      %Altura
+       0   1   0   0]      %Angulo de cabeceo
 
 D_c = [0]
 
 G = ss(A_c, B_c ,C_c , D_c)
 %Determinación de los tiempos de dinámica
-roots_c = eig(A_c);
-tR = abs(log(0.95)/real(roots_c(4)))
+roots_c = eig(A_c)
+tR = abs(log(0.95)/real(roots_c(2)));
 
 %%
 %SISTEMA DISCRETIZADO
-Tm = tR/4;
+Tm = .1;
 
 G_d = c2d(G, Tm , 'zoh');
 A_d = G_d.a
-B_d = G_d.d
+B_d = G_d.b
 C_d = G_d.c
 D_d = G_d.d
+%%
+%SISTEMA AMPLIADO
+Aa = [A_d , zeros(4,1) ; -C_d(1,:)*A_d, eye(1)];
+Ba = [B_d; -C_d(1,:)*B_d];
 
 %%
 %Prueba de Controlabilidad - Alcanzabilidad
-Ma = [B_d  A_d*B_d  A_d^2*B_d A_d^3*B_d];
-rank(Ma)
-Mc = [B_d  A_d*B_d  A_d^2*B_d A_d^3*B_d A_d^3];
+Mc = [Ba  Aa*Ba  Aa^2*Ba Aa^3*Ba Aa^4*Ba];
 rank(Mc)
+%%
+%CONTROLADOR
+coef_A = poly(Aa)
+
+W = [coef_A(5) coef_A(4) coef_A(3) coef_A(2) 1;
+     coef_A(4) coef_A(3) coef_A(2) 1         0;
+     coef_A(3) coef_A(2) 1         0         0;
+     coef_A(2) 1         0         0         0;
+     1         0         0         0         0];
+ 
+T = Mc*W;
+p1 = -15+15*i; p2 = -15-15*i ;p3 = -0.5+0.5*i ;p4 = -0.5-0.5*i;     %Polos en plano S
+p5 = -1;%Polo del integrador
+%Transformación Bilineal (plano z)
+polos_d = exp([p1 p2 p3 p4 p5]*Tm)
+alfa_i = poly(polos_d)
+
+K = fliplr([alfa_i(2:end) - coef_A(2:end)])*inv(T)
+
+Ki = -K(end)
+K  = K(1:4)
+
+%%
+%OBSERVADOR
+%SISTEMA DUAL
+Ao = A_d';
+Bo = C_d';
+Co = B_d';
+
+do = [1 1 1 1];
+Qo = diag(do);
+Ro = diag([1 1]);
+Ko = dlqr(Ao, Bo, Qo, Ro)'
+
+%%
+%SIMULACIÓN
+Tf = 70;            %Tiempo Final para la Simulación
+Ti = 0.001;        	%Tiempo de Integración Euler
+N  = ceil(Tf/Ti)    %Numero de puntos a simular
+deathZone = 0.5;
+
+
+t       = 0:Ti:N*Ti;
+u       = zeros(1, N+1);
+alpha   = zeros(1, N+1);
+phi     = zeros(1, N+1);
+phi_p   = zeros(1, N+1);
+h       = zeros(1, N+1);
+
+%CONDICIONES INICIALES
+alpha(1) = 0;
+phi(1) = 0;
+phi_p(1) = 0;
+h(1) = 500;
+x = [alpha(1) phi(1) phi_p(1) h(1)]';
+x_obs = [0 0 0 0]';
+ref = 100;
+psita = 0;
+
+um = 0;
+SAMPLE_T = floor(Tm/Ti);
+for i=1: N
+    
+    if(SAMPLE_T == 0)
+        
+        y_obs = C_d*x_obs;
+        y = C_d*x;
+    
+        psita_p = ref - y(1);
+        psita = psita + psita_p;
+    
+        %um = -K*x + Ki*psita;         %Sin Observador
+        um = -K*x_obs + Ki*psita;     %Con Observador
+    
+        %No linealidad del Actuador
+        if(abs(um)<deathZone)
+            um = 0;
+        else
+            um = sign(um)*(abs(um) - deathZone);
+        end
+    end
+    u(i) = um;
+    
+    %SISTEMA
+    alpha_p = a*(phi(i) - alpha(i));
+    phi_pp = -w^2*(phi(i) - alpha(i) -b*u(i));
+    h_p = c*alpha(i);
+    alpha(i+1) = alpha(i) + Ti*alpha_p;
+    phi_p(i+1) = phi_p(i) + Ti*phi_pp;
+    phi(i+1) = phi(i) + Ti*phi_p(i);
+    h(i+1) = h(i) + Ti*h_p;
+    
+    x = [alpha(i+1) phi(i+1) phi_p(i+1) h(i+1)]';
+    
+    if(SAMPLE_T == 0)
+       x_obs = A_d*x_obs + B_d*u(i) +  Ko*(y - y_obs);
+       SAMPLE_T = floor(Tm/Ti);
+    end
+    SAMPLE_T = SAMPLE_T-1;
+end
+
+%%
+%GRÁFICAS
+figure;
+subplot(3,1,1);
+plot(t, h, 'LineWidth',1.5); title('Altura del avión'); xlabel('Tiempo [s]'); ylabel('Altura [m]');
+grid on;
+subplot(3,1,2);
+plot(t, phi, 'LineWidth',1.5); title('Cabeceo'); xlabel('Tiempo [s]'); ylabel('Ángulo de Cabeceo [Rad]');
+grid on;
+subplot(3,1,3);
+plot(t, u, 'LineWidth',1.5); title('Acción de control'); xlabel('Tiempo [s]'); ylabel('u');
+grid on;
--- ActividadPrácticaN2/Item3.m
+++ ActividadPrácticaN2/Item3.m
@@ -1,27 +1,27 @@
 %% Sistemas de Control II -FCEFyN-UNC 
 % Profesor: Dr.Ing. Pucheta, Julian
 % Alumno: Mouton Laudin, Alfonso
-% Tp N¬∞ 2 - ITEM 2 - 
+% Tp N∞ 2 - ITEM 2 - 
 %
-% Calcular sistema controlador que haga evolucionar al p√©ndulo en el equilibrio estable. 
-%Objetivo de control: partiendo de una condici√≥n inicial nula en el desplazamiento y el √°ngulo en pi, hacer 
+% Calcular sistema controlador que haga evolucionar al pÈndulo en el equilibrio estable. 
+%Objetivo de control: partiendo de una condiciÛn inicial nula en el desplazamiento y el ·ngulo en pi, hacer 
 %que el carro se desplace a 10 metros evitando las oscilaciones de la masa m, considerando que es una 
-%gr√∫a. Una vez que el desplazamiento=10 modificar a m a un valor 10 veces mayor y volver al origen evitando oscilaciones.
-%  -Considerar que s√≥lo puede medirse el desplazamiento y el √°ngulo. 
+%gr˙a. Una vez que el desplazamiento=10 modificar a m a un valor 10 veces mayor y volver al origen evitando oscilaciones.
+%  -Considerar que sÛlo puede medirse el desplazamiento y el ·ngulo. 
 %  -Especificar el rango posible para el tiempo de muestreo para implementar el sistema en un 
 % microcontrolador.  
-%  -Determinar el efecto de la nolinealidad en la acci√≥n de control, descripta en la Fig. 4, y verificar cu√°l 
-% es el m√°ximo valor admisible de √©sa no linealidad.
+%  -Determinar el efecto de la nolinealidad en la acciÛn de control, descripta en la Fig. 4, y verificar cu·l 
+% es el m·ximo valor admisible de Èsa no linealidad.
 %%
 clc; clear all; close all;
 
-%Par√°metros del p√©ndulo
+%Par·metros del pÈndulo
 m1  = 0.1; m2 = m1*10; F  = 0.1; l  = 1.6; g  = 9.8; M  = 1.5;
-%MODELADO EN EE (Cont√≠nuo)
+%MODELADO EN EE (ContÌnuo)
 %1er Recorrido (0-10):
 A_c1 = [0     1               0          0;       %x1 = delta - desplazamiento
         0    -F/M            -m1*g/M     0;       %x2 = delta_p
-        0       0               0        1;       %x3 = phi - √°ngulo
+        0       0               0        1;       %x3 = phi - ·ngulo
         0   -F/(l*M)     -g*(m1+M)/(l*M) 0]       %x4 = phi_p
 
 %2do Recorrido (10-0):
@@ -44,13 +44,13 @@ D_c = [0;
 G_c1 = ss(A_c1 ,B_c ,C_c ,D_c);        % m1
 G_c2 = ss(A_c2 ,B_c ,C_c ,D_c);        % m2
 %%
-%C√°lculo de las din√°micas del sistema:
+%C·lculo de las din·micas del sistema:
 roots_c1 = real(eig(A_c1))
 roots_c2 = real(eig(A_c2))
 
-% Din√°mica r√°pida
+% Din·mica r·pida
 tR   = log(0.95)/roots_c1(2) 
-% Din√°mica lenta
+% Din·mica lenta
 tL   = log(0.05)/roots_c1(3) 
 %%
 %SISTEMA DISCRETIZADO
@@ -123,16 +123,16 @@ do2 = [1 50 500 .1];
 Qo2 = diag(do2); 
 Ro2 = diag([.1 .1]);
       
-% C√°lculo del Observador
+% C·lculo del Observador
 Ko1 = (dlqr(Ao1 ,Bo1 ,Qo1 ,Ro1))';
 Ko2 = (dlqr(Ao2 ,Bo2 ,Qo2 ,Ro2))';
 
 %%
-%SIMULACI√ìN
-Tf = 20;                %Tiempo Final de la simulaci√≥n
-%Ti = 1e-4;              %Tiempo de Integraci√≥n Euler
+%SIMULACI”N
+Tf = 20;                %Tiempo Final de la simulaciÛn
+%Ti = 1e-4;              %Tiempo de IntegraciÛn Euler
 Ti = Tm/20
-N = ceil(Tf/Ti)         %N√∫mero de puntos a simular
+N = ceil(Tf/Ti)         %N˙mero de puntos a simular
 deathZone = 0.5;        %Zona muerta del actuador
 psita = 0;
 d_pp = 0;
@@ -165,15 +165,15 @@ m = m1;
 ref = 10;
 f = 0;
 
-%Como el tiempo de integracion mucho m√°s bajo que el tiempo de
-%muestreo,actualizo el valor del controlador cada periodo de integraci√≥n K
+%Como el tiempo de integracion mucho m·s bajo que el tiempo de
+%muestreo,actualizo el valor del controlador cada periodo de integraciÛn K
 SAMPLE_T = floor(Tm/Ti);
 um = 0;
 y = [0 0]';
 y_obs = [0 0]';
 for i=1 : N
     
-    %Actualizo la acci√≥n de control por cada per√≠odo de muestreo
+    %Actualizo la acciÛn de control por cada perÌodo de muestreo
     if(SAMPLE_T == 0)
 
         y = C_d1*x;
@@ -227,29 +227,29 @@ for i=1 : N
 end
 
 %%
-%GR√ÅFICOS
+%GR¡FICOS
 
 figure(1);
 subplot(3,2,1); grid on; hold on;
 plot(t,phi_p,'LineWidth',1.5);grid on; title('Velocidad angular \phi_p');
 
 subplot(3,2,2); grid on; hold on;
-plot(t,phi,'LineWidth',1.5); title('√Ångulo \phi');xlabel('Tiempo');
+plot(t,phi,'LineWidth',1.5); title('¡ngulo \phi');xlabel('Tiempo');
 
 subplot(3,2,3); grid on; hold on;
-plot(t,d,'LineWidth',1.5);title('Posici√≥n gr√∫a \delta');xlabel('Tiempo');
+plot(t,d,'LineWidth',1.5);title('PosiciÛn gr˙a \delta');xlabel('Tiempo');
 
 subplot(3,2,4); grid on; hold on;
-plot(t,d_p,'LineWidth',1.5);title('Velocidad de gr√∫a \delta_p');
+plot(t,d_p,'LineWidth',1.5);title('Velocidad de gr˙a \delta_p');
 
 subplot(3,1,3); grid on; hold on;
-plot(t,u,'LineWidth',1.5);title('Acci√≥n de control u');xlabel('Tiempo en Seg.');
+plot(t,u,'LineWidth',1.5);title('AcciÛn de control u');xlabel('Tiempo en Seg.');
  
 figure(2);
 subplot(2,1,1);grid on; hold on;
 plot(phi,phi_p,'LineWidth',1.5);
-title('√Ångulo vs Velocidad angular');
-xlabel('√Ångulo');ylabel('Velocidad angular');
+title('¡ngulo vs Velocidad angular');
+xlabel('¡ngulo');ylabel('Velocidad angular');
  
 subplot(2,1,2);grid on; hold on;
 plot(d,d_p,'LineWidth',1.5);
